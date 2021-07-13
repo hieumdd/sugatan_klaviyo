4086df8d96646f755f2ce439799c87dcf5020ea7
HM <hieumdd@gmail.com>
2021-07-13 20:08:58 +0700

    Updated pipfile

--- test/test_manual.py
+++ test/test_manual.py
@@ -11,7 +11,7 @@ def test_metrics_manual():
         "client_name": CLIENT_NAME,
         "private_key": os.getenv(f"{CLIENT_NAME}_PRIVATE_KEY"),
         "mode": "metrics",
-        "start": "2021-06-01",
-        "end": "2021-06-30",
+        "start": "2021-07-01",
+        "end": "2021-07-13",
     }
     process(data)
